36b8c02c01814ef0ab1b8f3e0abf2e7edee774fc
Basil Ndonga <basilndonga@gmail.com>
2024-09-03 09:15:35 +0300

    fixed build issues

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': resolve(__dirname, './src')
+    }
   },
+  server: {
+    port: 3000,
+  }
 })
